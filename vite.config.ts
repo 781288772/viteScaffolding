33f1bb29afb9d18f63aa23655c036369b7a83209
这该死的魅力 <llb@163.com>
2021-12-16 16:36:03 +0800

    修改vite配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue()]
+// })
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path' // 编辑器提示 path 模块找不到，可以yarn add @types/node --dev
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src') // 设置 `@` 指向 `src` 目录
+    }
+  },
+  server: {
+    port: 3000, // 设置服务启动端口号
+    open: true, // 设置服务启动时是否自动打开浏览器
+    // 代理
+    proxy: {
+      '/api': {
+        target: 'http://API网关所在域名',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
+    }
+  }
 })
